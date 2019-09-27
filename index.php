2672d60b2df2c779c7fc30ad79b9912e37338305
Gabriel Vargas <gabriel.sv@grad.ufsc.br>
2019-09-27 13:42:20 -0300

    update col

--- index.php
+++ index.php
@@ -6,29 +6,46 @@
     <script src="lib/bootstrap/js/bootstrap.js"></script>
     <title>Gabriel Vargas</title>
     <link rel="stylesheet" href="lib/css/estilo.css">
+    <link rel="stylesheet" href="lib/font-awesome/css/fontawesome.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
+          integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width">
 </head>
 
+
+
 <body>
-<div id="interface">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center"><img class="img-fluid" style="margin-top: 15%;" src="img/html.png"></div>
-            <div class="col-md-12 text-center"><p><h1>Gabriel Vargas</h1></p></div>
-<!--            <div class="col-md-12 text-center"><img src="img/html.png" class="icones"></div>-->
+<div class="container-fluid" id="interface">
+    <div class="row">
+        <div style="font-size: 100px; margin-top: 15%; color: #606060" class="text-center col-md-12"><i
+                    class="fas fa-laptop "></i></div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center home-contatent">
+            <h1> Hi,I'm Gabriel Vargas.</h1>
         </div>
     </div>
-    <div >
-        <svg class="imagem-v width-v" preserveAspectRatio="none" viewBox="0 0 100 102" height="75"  style="left:-20px; bottom: -2px;">
-            <path d="M0 0 L50 95 L100 0 L100 100 L0 100 Z" fill="#040204" stroke="#040204"></path>
-        </svg>
-
+    <div class="row">
+        <div class="col-md-12 text-center home-contatent" style="margin-bottom: 10px; margin-top: -20px"><p>
+            <h1> I'm a programmer</h1></p></div>
     </div>
 </div>
 
-<div class="col-md-12 text-center segunda-tela" >
-   <div class="col-md-12" style="padding-bottom: 75px"></div>
-<!--    <div><div class="col-md-12 text-center"><img src="img/html.png" class="icones"></div></div>-->
+<!--        <svg class="imagem-v width-v" preserveAspectRatio="none" viewBox="0 0 100 102" height="75"  style="left:-19px; bottom: -2px;">-->
+<!--            <path d="M0 0 L50 95 L100 0 L100 100 L0 100 Z" fill="#606060" stroke="#606060"></path>-->
+<!--        </svg>-->
+<!--SEGUNDA TELA-->
+<div class="col-md-12  segunda-tela" >
+    <div class="container">
+
+        <div class="col-md-12" style="padding-bottom: 75px"></div>
+        <div class="col-md-12 text-center "><h1>About</h1></div>
+        <div>
+
+        </div>
+        <!--    <div class="grid grid-3-4 col-gap-2 grid-middle"> <img src="img/prog.png"></div>-->
+    </div>
 </div>
+
 </body>
 </html>
