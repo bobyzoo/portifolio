a187f8fb88b84a7393df9e3ba959410366f22945
Gabriel Vargas <gabriel.sv@grad.ufsc.br>
2019-09-27 09:23:10 -0300

    commit 1

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.css">
+    <script src="lib/bootstrap/js/bootstrap.js"></script>
+    <title>Gabriel Vargas</title>
+    <link rel="stylesheet" href="lib/css/estilo.css">
+    <meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+<div id="interface">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center"><img class="img-fluid" style="margin-top: 15%;" src="img/html.png"></div>
+            <div class="col-md-12 text-center"><p><h1>Gabriel Vargas</h1></p></div>
+<!--            <div class="col-md-12 text-center"><img src="img/html.png" class="icones"></div>-->
+        </div>
+    </div>
+    <div >
+        <svg class="imagem-v width-v" preserveAspectRatio="none" viewBox="0 0 100 102" height="75"  style="left:-20px; bottom: -2px;">
+            <path d="M0 0 L50 95 L100 0 L100 100 L0 100 Z" fill="#040204" stroke="#040204"></path>
+        </svg>
+
+    </div>
+</div>
+
+<div class="col-md-12 text-center segunda-tela" >
+   <div class="col-md-12" style="padding-bottom: 75px"></div>
+<!--    <div><div class="col-md-12 text-center"><img src="img/html.png" class="icones"></div></div>-->
+</div>
+</body>
+</html>
