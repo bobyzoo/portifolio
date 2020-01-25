267c13f8476da48bd621e9b32a936f3480098533
gabriel <gabrieldossantosvargas@gmail.com>
2020-01-25 18:16:40 -0300

    master 1.2.0
    Colapso in project

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
           integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <link rel="stylesheet" href="lib/css/animate.css">
-    <link rel="shortcut icon" href="img/icon.png" />
+    <link rel="shortcut icon" href="img/icon.png"/>
     <meta name="viewport" content="width=device-width">
 </head>
 <body>
@@ -47,7 +47,7 @@
         <div class="col-md-12"><h1
                     data-wow-offset='100'
                     class="wow fadeInLeftBig text-center font-weight-bold col-xl-4 offset-xl-4 col-lg-4 offset-lg-4 col-md-4 offset-md-4 col-sm-6 offset-sm-3 col-8 offset-2">
-                About</h1></div>
+                About me</h1></div>
     </div>
 
 
@@ -68,29 +68,28 @@
                 <!-- TITULO SOBRE-->
                 <h2 class="text-center offset-xl-2 col-xl-10 offset-lg-2 col-lg-8 offset-md-3 col-md-6 offset-sm-2 col-sm-8
         offset-2 col-8" style="margin-bottom: 10px;">
-                    Who I am?</h2>
+                    Who am I ?</h2>
                 <!--  TEXTO SOBRE-->
-                <div id="sobre-text" class="text-center offset-md-2"><p>Full-Stack Developer, I seek to be updated on
-                        technology and systems development practices.
-                        Self-taught, creative, proactive, I solve problems well, code review, well coded and well
+                <div id="sobre-text" class="text-justify offset-md-2 "><p>I'm a self-taught full-stack developer seeking
+                        to growing up in tech and systems development.
+                        Creative, proactive, I solve problems well, code review, well coded and well
                         commented.
-
                     </p>
-                    <p>Graduated in Technician in Electronics, pursuing a degree in Computer Science and Digital Games.
+                    <p>Graduated in Technician in Electronics - IFSC, pursuing a degree in Computer Science - UFSC and Digital Games - Estácio.
                     </p>
                 </div>
             </div>
 
 
-            <!-- DIV GRÁFICOS-->
+            <!-- DIV Graphics-->
             <div class=" wow slideInUp conteudo-graficos col-lg-5 offset-lg-1">
-                <!-- TITULO GRAFICOS-->
+                <!-- title Graphics-->
                 <div class=" col-md-10 offset-md-1 col-10 offset-1 text-center principal-skills">
-                    <h2>Principal Skills</h2>
+                    <h2>Programming Languages</h2>
                 </div>
-                <!--  GRAFICOS-->
+                <!--  Graphics-->
                 <div class="graficos col-lg-12">
-                    <!--                        Graficos-->
+                    <!--Graphics-->
                     <div class='row'>
                         <div class='title-graphic'>HTML
                         </div>
@@ -151,7 +150,7 @@
                             </div>
                         </div>
                     </div>
-                    <!--                        Fim grafico-->
+                    <!--                        Fim Graphics-->
                 </div>
             </div>
 
@@ -270,7 +269,7 @@
                 <div class="text-center wow fadeInRight col-xl-5 offset-xl-3 col-6 offset-3 ferramentas"><h2>Tools</h2>
                 </div>
 
-                <div class="col-lg-12  wow fadeInUp ">
+                <div class="col-lg-12  wow fadeInUp data-wow-offset ">
                     <div class='row'>
                         <div class='title-graphic'>GitHub
                         </div>
@@ -355,7 +354,7 @@
                             </div>
                         </div>
                     </div>
-                    </divdata-wow-offset>
+                </div>
 
                 </div>
             </div>
@@ -389,81 +388,212 @@
 
             </div>
             <!-- Grid column -->
-
         </div>
-        <!-- Grid row -->
 
-        <!-- Grid row -->
         <div class="gallery" id="gallery">
-            <!-- Grid column -->
-            <div class="mb-3 pics  all 2">
+            <!-- Images-->
+            <div class="mb-3 pics imgs all 2" type="button" data-toggle="modal" data-target="#modal1">
                 <figure class="figure">
-                    <a target="_blank" href="http://app.geneslab.com.br/login.php"><img class="img-fluid figure-img rounded " src="img/geld.PNG" id="img-1"
-                            alt="Card image cap"></a>
+                    <img class="img-fluid figure-img rounded " src="img/geld.PNG" id="img-1" alt="Card image cap">
                     <figcaption class="figure-caption text-right">Geld - PHP/MySQL/HTML5/CSS/JS</figcaption>
                 </figure>
             </div>
-            <!-- Grid column -->
-            <div class="mb-3 pics  all 1">
+            <div class="mb-3 pics imgs all 1" type="button" data-toggle="modal" data-target="#modal2">
                 <figure class="figure">
-                    <a target="_blank" href="https://github.com/bobyzoo/ProjetoX"> <img src="img/github.jpg" id="img-2"
-                                                                        class="figure-img img-fluid rounded"
-                                                                        alt="Card image cap"></a>
+                    <img src="img/github.jpg" id="img-2" class="figure-img img-fluid rounded" alt="Card image cap">
                     <figcaption class="figure-caption text-right">Virtual Assistant - Python3/SQLite</figcaption>
                 </figure>
             </div>
-
-            <!-- Grid column -->
-            <div class="mb-3 pics  all 1">
+            <div class="mb-3 pics imgs all 1" type="button" data-toggle="modal" data-target="#modal3">
                 <figure class="figure">
-                    <a target="_blank" href="https://github.com/bobyzoo/game"> <img src="img/gameC.jpg" id="img-2"
-                                                                    class="figure-img img-fluid rounded"
-                                                                    alt="Card image cap"></a>
+                    <img src="img/gameC.jpg" id="img-3" class="figure-img img-fluid rounded" alt="Card image cap">
                     <figcaption class="figure-caption text-right">Sticker Figth Game - C++</figcaption>
                 </figure>
             </div>
-            <div class="mb-3 pics  all 1">
+            <div class="mb-3 pics imgs all 1" type="button" data-toggle="modal" data-target="#modal4">
                 <figure class="figure">
-                    <a target="_blank" href="https://github.com/bobyzoo/BattleOfTheSpirits"> <img src="img/gamePython.png" id="img-2"
-                                                                                  class="figure-img img-fluid rounded"
-                                                                                  alt="Card image cap"></a>
+                    <img src="img/gamePython.png" id="img-4" class="figure-img img-fluid rounded" alt="Card image cap">
                     <figcaption class="figure-caption text-right">Battle of the Spirits - Python3</figcaption>
                 </figure>
             </div>
+
         </div>
     </div>
-</div>
-    <footer id="footer" class="col-md-12 conteudo" style="margin-top: -8px">
-        <div class="container-fluid">
-            <div class="row" style="padding-top: 20px">
-                <div class="text-center col-md-12 footer-create" >
-                    Website created by &copy; Gabriel Vargas
 
+
+    <!-- Modals -->
+    <div class="modal fade" id="modal1" tabindex="-1" role="dialog"
+         aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Geld</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                <div class="text-center col-md-12">
-                    <a target="_blank" href="https://www.instagram.com/gabrieldsvr/" class="footer"><i
-                                class="fab fa-instagram footer" style="margin-right: 10px"></i></a>
-                    <a target="_blank" href="https://github.com/bobyzoo" class="footer"><i class="fab fa-github footer"
-                                                                                           style="margin-right: 10px"></i> </a>
-                    <a target="_blank" class="footer" href="https://www.linkedin.com/in/gabriel-dos-santos-vargas-3a1aa6119/"> <i
-                                class="fab fa-linkedin footer" style="margin-right: 10px"></i></a>
-                </div></div>
+                <div class="modal-body">
+                    <div>
+                        Business: GenesisGroup<br>
+                        Languages: PHP, CSS, HTML, JavaScript<br>
+                        Lib: JQuery, BootStrap, DataTables<br>
+                        Other technologies: AJAX, JSON<br>
+                        My contribution: Mar 2019 - Currently
+                    </div>
+                    <div style="border-top: 1px solid #dee2e6; margin-top:10px;padding: 10px 0px" >
+                        Resume:<br>
+                        A platform of solutions in tests, inspections, analyzes, certification and traceability for the agrifood chain.<br>
+                        I actively participate in the tasks of the IT team.<br>
+
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close
+                        </button>
+                        <button type="button" class="btn btn-primary"><a target="_blank"
+                                                                         style="color: white; text-decoration: none"
+                                                                         href="http://app.geneslab.com.br/login.php">Web Site</a></button>
+                        <button type="button" class="btn btn-primary"><a target="_blank"
+                                                                         style="color: white; text-decoration: none"
+                                                                         href="http://genesisgroup.publishouse.tecnologia.ws/">Company Web Site</a></button>
+                    </div>
+                </div>
+            </div>
         </div>
-    </footer>
+    </div>
+    <div class="modal fade" id="modal2" tabindex="-1" role="dialog"
+         aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Virtual Assistant</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div>
+                        My own project<br>
+                        Languages: Python3,SQLite<br>
+                        Lib: speech_recognition, cv2, requests, bs4, pyttsx3, htaccess</br>
+                        Other technologies: JSON<br>
+                        Creation date: 2018
+                    </div>
+                    <div style="border-top: 1px solid #dee2e6; margin-top:10px;padding: 10px 0px" >
+                        Resume:<br>
+                        Virtual assistant with facial and voice recognition. Responds with audio
 
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close
+                        </button>
+                        <button type="button" class="btn btn-primary"><a target="_blank"
+                                                                         style="color: white; text-decoration: none"
+                                                                         href="https://github.com/bobyzoo/ProjetoX">Web Site</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="modal3" tabindex="-1" role="dialog"
+         aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Sticker Figth Game</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div>
+                        My own project<br>
+                        Languages: C++<br>
+                        Lib: allegro5.h<br>
+                        Creation date: 2018
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close
+                        </button>
+                        <button type="button" class="btn btn-primary"><a target="_blank"
+                                                                         style="color: white; text-decoration: none"
+                                                                         href="https://github.com/bobyzoo/game">Web Site</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="modal4" tabindex="-1" role="dialog"
+         aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Battle of the Spirits</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div>
+                        My own project<br>
+                        Languages: Python3<br>
+                        Lib: pygame<br>
+                        Creation date: 2019
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close
+                        </button>
+                        <button type="button" class="btn btn-primary"><a target="_blank"
+                                                                         style="color: white; text-decoration: none"
+                                                                         href="https://github.com/bobyzoo/BattleOfTheSpirits">Web Site</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+<footer id="footer" class="col-md-12 conteudo" style="margin-top: -8px">
+    <div class="container-fluid">
+        <div class="row" style="padding-top: 20px">
+            <div class="text-center col-md-12 footer-create">
+                Website created by &copy; Gabriel Vargas
+
+            </div>
+            <div class="text-center col-md-12">
+                <a target="_blank" href="https://www.instagram.com/gabrieldsvr/" class="footer"><i
+                            class="fab fa-instagram footer" style="margin-right: 10px"></i></a>
+                <a target="_blank" href="https://github.com/bobyzoo" class="footer"><i class="fab fa-github footer"
+                                                                                       style="margin-right: 10px"></i>
+                </a>
+                <a target="_blank" class="footer"
+                   href="https://www.linkedin.com/in/gabriel-dos-santos-vargas-3a1aa6119/"> <i
+                            class="fab fa-linkedin footer" style="margin-right: 10px"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
-
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 <script src="lib/js/jquery-3.4.1.min.js"></script>
 <script src="lib/js/wow.min.js"></script>
 <script>
-    $('*').filter(function() {
+
+    $('*').filter(function () {
         return $(this).css('z-index') == 9999999;
-    }).each(function() {
+    }).each(function () {
         $(this).remove();
     });
-</script>
-<script>
-
 
     new WOW({
         mobile: false,
